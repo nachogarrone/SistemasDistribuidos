12fb4a9772d8bfff330beeb4375f437ae1b1518d
nachogarrone <nachogarrone@gmail.com>
2017-03-30 20:16:45 -0300

    Initial commit

---
+++ src/com/ucu/model/ClienteEntity.java
@@ -0,0 +1,83 @@
+package com.ucu.model;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by nachogarrone on 3/28/17.
+ */
+@Entity
+@Table(name = "Cliente", schema = "sistemas_distribuidos", catalog = "")
+public class ClienteEntity {
+    private int idCliente;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @Id
+    @Column(name = "idCliente", nullable = false)
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    @Basic
+    @Column(name = "firstName", nullable = false, length = 45)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Basic
+    @Column(name = "lastName", nullable = false, length = 45)
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Basic
+    @Column(name = "email", nullable = true, length = 100)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ClienteEntity that = (ClienteEntity) o;
+
+        if (idCliente != that.idCliente) return false;
+        if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(that.lastName) : that.lastName != null) return false;
+        if (email != null ? !email.equals(that.email) : that.email != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = idCliente;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
+}
